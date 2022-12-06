da8fc7781b4d0679762a435959c88fdeb7aae5a1
Anisaftr18 <83202465+Anisaftr18@users.noreply.github.com>
2022-12-06 14:12:08 +0700

    Update detail-product.js

--- Front-End/src/scripts/views/pages/detail-product.js
+++ Front-End/src/scripts/views/pages/detail-product.js
@@ -2,12 +2,11 @@
 import UrlParser from '../../routes/url-parser';
 import ProductDataSource from '../../data/productdb-source';
 import { createProductDetailTemplate } from '../templates/template-creator';
-// import LikeButtonPresenter from '../../utils/like-button-presenter';
 
 const DetailProduct = {
   async render() {
     return `
-            <div class="product" id="product"></div>
+            <div class="detail-products" id="detail-products"></div>
         `;
   },
 
@@ -15,7 +14,7 @@ const DetailProduct = {
     // document.querySelector('hero-element').style.display = 'none';
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const product = await ProductDataSource.getProductDetail(url.id);
-    const productContainer = document.querySelector('#product');
+    const productContainer = document.querySelector('#detail-products');
     productContainer.innerHTML = createProductDetailTemplate(product);
   },
 };
