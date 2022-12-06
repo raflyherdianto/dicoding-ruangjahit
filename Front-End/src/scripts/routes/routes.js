16e4fb3463baae797eeba59a8d2ecd4d99c6318c
Anisaftr18 <83202465+Anisaftr18@users.noreply.github.com>
2022-12-06 12:41:10 +0700

    add detail product, tailor, user-profile

--- Front-End/src/scripts/routes/routes.js
+++ Front-End/src/scripts/routes/routes.js
@@ -1,22 +1,26 @@
 import Home from '../views/pages/home';
 import Product from '../views/pages/product';
-import ProductCart from '../views/pages/product-cart';
 import Tailor from '../views/pages/tailor';
 import Favorite from '../views/pages/favorite';
 import AboutUs from '../views/pages/about-us';
 import SignIn from '../views/pages/sign-in';
 import SignUp from '../views/pages/sign-up';
+import DetailProduct from '../views/pages/detail-product';
+import UserProfile from '../views/pages/user-profile';
+import DetailTailor from '../views/pages/detail-tailor';
 
 const routes = {
   '/': Home, // default page
   '/home': Home,
   '/product': Product,
-  '/product-cart': ProductCart,
   '/tailor': Tailor,
   '/favorite': Favorite,
   '/about-us': AboutUs,
   '/sign-in': SignIn,
   '/sign-up': SignUp,
+  '/detail-product/:id': DetailProduct,
+  '/user-profile': UserProfile,
+  '/detail-tailor': DetailTailor,
 };
 
 export default routes;
