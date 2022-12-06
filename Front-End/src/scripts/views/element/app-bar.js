ac47e63b55ee0c0cdf7099639c369ea51a5d738d
Anisaftr18 <83202465+Anisaftr18@users.noreply.github.com>
2022-12-06 12:52:52 +0700

    update app-bar

--- Front-End/src/scripts/views/element/app-bar.js
+++ Front-End/src/scripts/views/element/app-bar.js
@@ -9,28 +9,43 @@ class AppBar extends HTMLElement {
   render() {
     this.innerHTML = `
     <header class="app-bar">
-      <div class="app-bar__brand">
-        <img src="./assets/logo-ruangjahit.png" width="150px" alt="Logo Ruang Jahit">
-      </div>
-      <nav id="navigationDrawer" class="app-bar__navigation">
-        <div>
-          <ul>
-              <li><a href="#/home" class="nav-link">Home</a></li>
-              <li><a href="#/product" class="nav-link">Product</a></li>
-              <li><a href="#/tailor" class="nav-link">Tailor</a></li>
-              <li><a href="#/favorite" class="nav-link">Favorite</a></li>
-              <li><a href="#/about-us" class="nav-link">About Us</a></li>
-
-              <li class="login"><a href="#/sign-in">Masuk</a></li>
-              <li class="register"><a href="#/sign-up">Daftar</a></li>
-          </ul>
-
-        </div>
-      </nav>
-      <div class="app-bar__menu">
-        <input type="checkbox" id="hamburgerButton">
-        <label for="hamburgerButton" class="btn-toggle">☰</label>
-      </div>
+        <nav class="navbar navbar-expand-lg app-bar__navigation" id="navigationDrawer">
+              <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+              <img src="./assets/logo-ruangjahit.png" alt="Bootstrap" width="150">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" id="hamburgerButton" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="">☰</span>
+              </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                      <a class="nav-link" aria-current="page" href="#/home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#/product">Product</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#/tailor">Tailor</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Favorite
+                      </a>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#/favorite">Favorite Product</a></li>
+                        <li><a class="dropdown-item" href="#/favorite">Favorite Tailors</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#/about-us">About Us</a>
+                    </li>
+                  </ul>
+                  <li class="login"><a href="#/sign-in" class="text-white">Masuk</a></li>
+                  <li class="register"><a href="#/sign-up" class="text-white">Daftar</a></li>
+                </div>
+              </div>
+        </nav>
     </header>
     `;
   }
