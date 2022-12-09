f12dc005ea1363bc58c029a4ac7c02b53f9340e1
nilammgfra22 <nilammagfira22@gmail.com>
2022-12-09 21:25:40 +0700

    update app-bar-user

--- Front-End/src/scripts/views/components/app-bar_user.js
+++ Front-End/src/scripts/views/components/app-bar_user.js
@@ -41,12 +41,64 @@ class AppBarUser extends HTMLElement {
                       <a class="nav-link" href="#/user-about-us">About Us</a>
                     </li>
                   </ul>
-                  <li class="btnLeft"><a href="#/user-product-cart" class="text-white"><i class="bi bi-bag-fill"></i></a></li>
+                  <li class="btnLeft"><a href="#/user-product-cart" class="text-white" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i class="bi bi-bag-fill"></i><sub id="count">  0</sub></a></li>
                   <li class="btnRight"><a href="#/user-profile" class="text-white"><i class="bi bi-person-circle"></i></a></li>
                 </div>
               </div>
         </nav>
     </header>
+    <!-- Filter Modal -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasRightLabel">My Cart</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+
+          <div class="offcanvas-body my-cart">
+            <div class="content-order-list">
+              <div class="card mb-4">
+                  <div class="card-head">
+                      <img class="card-img-left example-card-img-responsive" src="../product/1.png"/>
+                  </div>
+                  
+                  <div class="card-body">
+                      <h4 class="card-title h5 h4-sm">Blouse</h4>
+                      <p class="card-price">Harga : Rp. 80.000,-</p>
+                      <p class="card-quantity">Kuantitas : 1 pcs</p>
+                  </div>
+              </div>
+
+              <div class="card mb-4">
+                  <div class="card-head">
+                      <img class="card-img-left example-card-img-responsive" src="../product/1.png"/>
+                  </div>
+                  
+                  <div class="card-body">
+                      <h4 class="card-title h5 h4-sm">Blouse</h4>
+                      <p class="card-price">Harga : Rp. 80.000,-</p>
+                      <p class="card-quantity">Kuantitas : 1 pcs</p>
+                  </div>
+              </div>
+
+              <div class="card mb-4">
+                  <div class="card-head">
+                      <img class="card-img-left example-card-img-responsive" src="../product/1.png"/>
+                  </div>
+                  
+                  <div class="card-body">
+                      <h4 class="card-title h5 h4-sm">Blouse</h4>
+                      <p class="card-price">Harga : Rp. 80.000,-</p>
+                      <p class="card-quantity">Kuantitas : 1 pcs</p>
+                  </div>
+              </div>
+            </div>
+
+            <div class="total-order">
+                <p class="card-quantity">Total : Rp. 240.000,- </p>
+                <a href="#" class="btn-checkout">Lanjut Pembayaran</a>
+            </div>
+          </div>
+      </div>
     `;
   }
 }
